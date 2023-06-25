ca39856ab8b6173b3e36ca82316a685fee741c8a
karam.Fci <karam.fci@gmail.com>
2023-06-25 12:09:09 +0300

    jen commit1

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,26 +1,42 @@
-pipeline {
-    agent any 
-    tools {
-        maven "MAVEN-3.9.2"
-        jdk "ORACELJDK11"
+// pipeline {
+//     agent any 
+//     tools {
+//         maven "MAVEN-3.9.2"
+//         jdk "ORACELJDK11"
 
-    }
+//     }
 
-    stages {
-        stage('fetch code')
-            steps {
-                git branch :  'main' , url: 'https://github.com/karamFci/nexjen.git'
-            }
+//     stages {
+//         stage('fetch code')
+//             steps {
+//                 git branch :  'main' , url: 'https://github.com/karamFci/nexjen.git'
+//             }
         
-        stage('Build'){
-            steps{
-                sh 'mvn install -DskipTests'
-            }
-            post{ 
-                success {
-                    echo 'Now Archiving...'
-                }
-            }
+//         stage('Build'){
+//             steps{
+//                 sh 'mvn install -DskipTests'
+//             }
+//             post{ 
+//                 success {
+//                     echo 'Now Archiving...'
+//                 }
+//             }
+//         }
+//     }
+// }
+
+pipeline {
+	agent none
+  stages {
+  	stage('Maven Install') {
+    	agent {
+      	docker {
+        	image 'maven:3.5.0'
         }
+      }
+      steps {
+      	sh 'mvn clean install'
+      }
     }
+  }
 }
